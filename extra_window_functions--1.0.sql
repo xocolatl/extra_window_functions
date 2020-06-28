36935c085cb8ca0c34f5df4a11734a1dc7c849a0
Vik Fearing <vik.fearing@protonmail.com>
2020-06-28 02:06:38 +0200

    Remove remnant of development name

--- extra_window_functions--1.0.sql
+++ extra_window_functions--1.0.sql
@@ -1,5 +1,5 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION absinthe" to load this file. \quit
+\echo Use "CREATE EXTENSION extra_window_functions" to load this file. \quit
 
 /* LAG */
 
