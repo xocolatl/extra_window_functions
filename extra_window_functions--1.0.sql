7ad482c372bc39ad28aa852ddeb72299ac384b06
Vik Fearing <vik.fearing@protonmail.com>
2020-06-28 02:11:44 +0200

    Make the extension relocatable
    (like it always should have been...)

--- extra_window_functions--1.0.sql
+++ extra_window_functions--1.0.sql
@@ -3,19 +3,19 @@
 
 /* LAG */
 
-CREATE OR REPLACE FUNCTION public.lag_ignore_nulls(anyelement)
+CREATE OR REPLACE FUNCTION lag_ignore_nulls(anyelement)
  RETURNS anyelement
  LANGUAGE c
  WINDOW IMMUTABLE PARALLEL SAFE STRICT
 AS 'MODULE_PATHNAME', 'window_lag_ignore_nulls';
 
-CREATE OR REPLACE FUNCTION public.lag_ignore_nulls(anyelement, integer)
+CREATE OR REPLACE FUNCTION lag_ignore_nulls(anyelement, integer)
  RETURNS anyelement
  LANGUAGE c
  WINDOW IMMUTABLE PARALLEL SAFE STRICT
 AS 'MODULE_PATHNAME', 'window_lag_ignore_nulls_with_offset';
 
-CREATE OR REPLACE FUNCTION public.lag_ignore_nulls(anyelement, integer, anyelement)
+CREATE OR REPLACE FUNCTION lag_ignore_nulls(anyelement, integer, anyelement)
  RETURNS anyelement
  LANGUAGE c
  WINDOW IMMUTABLE PARALLEL SAFE STRICT
@@ -23,19 +23,19 @@ AS 'MODULE_PATHNAME', 'window_lag_ignore_nulls_with_offset_with_default';
 
 /* LEAD */
 
-CREATE OR REPLACE FUNCTION public.lead_ignore_nulls(anyelement)
+CREATE OR REPLACE FUNCTION lead_ignore_nulls(anyelement)
  RETURNS anyelement
  LANGUAGE c
  WINDOW IMMUTABLE PARALLEL SAFE STRICT
 AS 'MODULE_PATHNAME', 'window_lead_ignore_nulls';
 
-CREATE OR REPLACE FUNCTION public.lead_ignore_nulls(anyelement, integer)
+CREATE OR REPLACE FUNCTION lead_ignore_nulls(anyelement, integer)
  RETURNS anyelement
  LANGUAGE c
  WINDOW IMMUTABLE PARALLEL SAFE STRICT
 AS 'MODULE_PATHNAME', 'window_lead_ignore_nulls_with_offset';
 
-CREATE OR REPLACE FUNCTION public.lead_ignore_nulls(anyelement, integer, anyelement)
+CREATE OR REPLACE FUNCTION lead_ignore_nulls(anyelement, integer, anyelement)
  RETURNS anyelement
  LANGUAGE c
  WINDOW IMMUTABLE PARALLEL SAFE STRICT
@@ -43,13 +43,13 @@ AS 'MODULE_PATHNAME', 'window_lead_ignore_nulls_with_offset_with_default';
 
 /* FIRST_VALUE */
 
-CREATE OR REPLACE FUNCTION public.first_value_ignore_nulls(anyelement)
+CREATE OR REPLACE FUNCTION first_value_ignore_nulls(anyelement)
  RETURNS anyelement
  LANGUAGE c
  WINDOW IMMUTABLE PARALLEL SAFE STRICT
 AS 'MODULE_PATHNAME', 'window_first_value_ignore_nulls';
 
-CREATE OR REPLACE FUNCTION public.first_value_ignore_nulls(anyelement, anyelement)
+CREATE OR REPLACE FUNCTION first_value_ignore_nulls(anyelement, anyelement)
  RETURNS anyelement
  LANGUAGE c
  WINDOW IMMUTABLE PARALLEL SAFE STRICT
@@ -57,13 +57,13 @@ AS 'MODULE_PATHNAME', 'window_first_value_ignore_nulls_with_default';
 
 /* LAST_VALUE */
 
-CREATE OR REPLACE FUNCTION public.last_value_ignore_nulls(anyelement)
+CREATE OR REPLACE FUNCTION last_value_ignore_nulls(anyelement)
  RETURNS anyelement
  LANGUAGE c
  WINDOW IMMUTABLE PARALLEL SAFE STRICT
 AS 'MODULE_PATHNAME', 'window_last_value_ignore_nulls';
 
-CREATE OR REPLACE FUNCTION public.last_value_ignore_nulls(anyelement, anyelement)
+CREATE OR REPLACE FUNCTION last_value_ignore_nulls(anyelement, anyelement)
  RETURNS anyelement
  LANGUAGE c
  WINDOW IMMUTABLE PARALLEL SAFE STRICT
@@ -71,43 +71,43 @@ AS 'MODULE_PATHNAME', 'window_last_value_ignore_nulls_with_default';
 
 /* NTH_VALUE */
 
-CREATE OR REPLACE FUNCTION public.nth_value(anyelement, integer, anyelement)
+CREATE OR REPLACE FUNCTION nth_value(anyelement, integer, anyelement)
  RETURNS anyelement
  LANGUAGE c
  WINDOW IMMUTABLE PARALLEL SAFE STRICT
 AS 'MODULE_PATHNAME', 'window_nth_value_with_default';
 
-CREATE OR REPLACE FUNCTION public.nth_value_from_last(anyelement, integer)
+CREATE OR REPLACE FUNCTION nth_value_from_last(anyelement, integer)
  RETURNS anyelement
  LANGUAGE c
  WINDOW IMMUTABLE PARALLEL SAFE STRICT
 AS 'MODULE_PATHNAME', 'window_nth_value_from_last';
 
-CREATE OR REPLACE FUNCTION public.nth_value_from_last_ignore_nulls(anyelement, integer)
+CREATE OR REPLACE FUNCTION nth_value_from_last_ignore_nulls(anyelement, integer)
  RETURNS anyelement
  LANGUAGE c
  WINDOW IMMUTABLE PARALLEL SAFE STRICT
 AS 'MODULE_PATHNAME', 'window_nth_value_from_last_ignore_nulls';
 
-CREATE OR REPLACE FUNCTION public.nth_value_from_last_ignore_nulls(anyelement, integer, anyelement)
+CREATE OR REPLACE FUNCTION nth_value_from_last_ignore_nulls(anyelement, integer, anyelement)
  RETURNS anyelement
  LANGUAGE c
  WINDOW IMMUTABLE PARALLEL SAFE STRICT
 AS 'MODULE_PATHNAME', 'window_nth_value_from_last_ignore_nulls_with_default';
 
-CREATE OR REPLACE FUNCTION public.nth_value_from_last(anyelement, integer, anyelement)
+CREATE OR REPLACE FUNCTION nth_value_from_last(anyelement, integer, anyelement)
  RETURNS anyelement
  LANGUAGE c
  WINDOW IMMUTABLE PARALLEL SAFE STRICT
 AS 'MODULE_PATHNAME', 'window_nth_value_from_last_with_default';
 
-CREATE OR REPLACE FUNCTION public.nth_value_ignore_nulls(anyelement, integer)
+CREATE OR REPLACE FUNCTION nth_value_ignore_nulls(anyelement, integer)
  RETURNS anyelement
  LANGUAGE c
  WINDOW IMMUTABLE PARALLEL SAFE STRICT
 AS 'MODULE_PATHNAME', 'window_nth_value_ignore_nulls';
 
-CREATE OR REPLACE FUNCTION public.nth_value_ignore_nulls(anyelement, integer, anyelement)
+CREATE OR REPLACE FUNCTION nth_value_ignore_nulls(anyelement, integer, anyelement)
  RETURNS anyelement
  LANGUAGE c
  WINDOW IMMUTABLE PARALLEL SAFE STRICT
@@ -115,13 +115,13 @@ AS 'MODULE_PATHNAME', 'window_nth_value_ignore_nulls_with_default';
 
 /* FLIP_FLOP */
 
-CREATE OR REPLACE FUNCTION public.flip_flop(boolean)
+CREATE OR REPLACE FUNCTION flip_flop(boolean)
  RETURNS boolean
  LANGUAGE c
  WINDOW IMMUTABLE PARALLEL SAFE STRICT
 AS 'MODULE_PATHNAME', 'window_flip_flop_1';
 
-CREATE OR REPLACE FUNCTION public.flip_flop(boolean, boolean)
+CREATE OR REPLACE FUNCTION flip_flop(boolean, boolean)
  RETURNS boolean
  LANGUAGE c
  WINDOW IMMUTABLE PARALLEL SAFE STRICT
